ddcd4293393962662f70400bb3b09186ca37b420
Ch333f <adewale.o.joshua@gmail.com>
2024-09-20 02:01:21 +0100

    Implemented backend server

---
+++ backend/server.py
@@ -0,0 +1,31 @@
+from flask import Flask
+from flask_cors import CORS
+from flask_restx import Api
+
+from resources import admin
+from dbmodel import db
+
+
+cowrywisebea_app = Flask(__name__)
+cors = CORS()
+api = Api(
+    title="CowrywiseBEA",
+    version="Beta v0.1.0",
+    description="Cowrywise Backend Engineer (Infrastructure, API Engineer, Devops) Assessment backend API for admin management",
+    doc="/",
+)
+
+cowrywisebea_app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///CowrywiseBEAAdminDataStore.db"
+cowrywisebea_app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db.init_app(cowrywisebea_app)
+api.init_app(cowrywisebea_app)
+cors.init_app(cowrywisebea_app)
+
+api.add_namespace(admin)
+
+if __name__ == "__main__":
+    cowrywisebea_app.run(
+        debug=True,
+        port=5001,
+    )
